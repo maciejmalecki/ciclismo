6069379023543dc42fb93829f9a69e822734558d
Maciej Malecki <malecki.maciej@gmail.com>
2021-04-14 22:48:31 +0200

    Configure asciidoctor (1)

---
+++ doc/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("org.asciidoctor.jvm.convert") version "3.1.0"
+}
+
+tasks {
+    "asciidoctor"(org.asciidoctor.gradle.jvm.AsciidoctorTask::class) {
+        sourceDir("src")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "ciclismo"
+
+include(":doc")
